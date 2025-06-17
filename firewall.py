766880b78724f6c404628065717f06b0f7035d2d
Matias <mvaleriani@fi.uba.ar>
2025-06-17 18:54:55 -0300

    Corrijo logs

--- firewall.py
+++ firewall.py
@@ -19,10 +19,11 @@ import pox.lib.packet as pkt
 import json
 
 COLOR_CODES = {
-    'BLUE': '\033[94m',
+    'CYAN': '\033[96m',
+    'MAGENTA': '\033[95m',
     'GREEN': '\033[92m',
-    'YELLOW': '\033[93m',
-    'RED': '\033[91m',
+    'GREY': '\033[90m',
+    'BLACK': '\033[30m',
     "RESET": '\033[0m'
 }
 
@@ -77,13 +78,13 @@ class Firewall (EventMixin):
 
         (protocol, src_port, dst_port) = self.__get_destination(packet)
 
-        sender = COLOR_CODES["BLUE"] + \
+        sender = COLOR_CODES["CYAN"] + \
             str(packet.srcip) + f':{src_port}' + COLOR_CODES["RESET"]
-        receiver = COLOR_CODES["YELLOW"] + \
+        receiver = COLOR_CODES["MAGENTA"] + \
             str(packet.dstip) + f':{dst_port}' + COLOR_CODES["RESET"]
 
         dpid = COLOR_CODES['GREEN'] + str(event.dpid) + COLOR_CODES['RESET']
-        protocol = COLOR_CODES['RED'] + protocol + COLOR_CODES['RESET']
+        protocol = COLOR_CODES['GREY'] + protocol + COLOR_CODES['RESET']
 
         log.info(f"{dpid}:Packet: {protocol} {sender + ' --> ' + receiver}")
 
